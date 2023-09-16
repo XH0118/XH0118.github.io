3eb0e51cf5eddc82654bdbad3f9343593390c0e6
XH0118 <2364546766@qq.com>
2023-09-16 21:48:55 +0800

    Site updated: 2023-09-16 21:48:53

---
+++ download/Birthday/config.js
@@ -0,0 +1,65 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "最好的同桌",  // 同上...
+        "今天是你的生日",
+        "这是我们认识以来",
+        "你的又一个生日！",
+        "这五年来，很幸运遇见你",
+	"敬相遇，敬我们的友谊",
+        "我们都要20+了，真快啊。",
+        "在这样特殊的日子里",
+        "依然祝你生日快乐，每天开心！",
+        "做那个依旧开心的安佳乐",
+        "happy birthday！",
+        "新的一岁要吃好好的",
+        "喂饱饱的",
+        "然后才有力气学习和生活！",
+        "生日快乐~~",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "送给我": "./imgs/message.png",
+        "最好的同桌": "./imgs/mate.jpg",
+        "今天是你的生日": "./imgs/birthday.png",
+        "这是我们认识以来": "./imgs/begin.jpg",
+        "你的又一个生日！": "./imgs/shengri.jpeg",
+        "这五年来，很幸运遇见你": "./imgs/meet.jpg",
+        "敬相遇，敬我们的友谊": "./imgs/friend.jpg",
+        "我们都要20+了，真快啊。": "./imgs/num20.png",
+        "在这样特殊的日子里": "./imgs/p1.jpg",
+        "依然祝你生日快乐，每天开心！": "./imgs/p2.jpg",
+        "做那个依旧开心的安佳乐": "./imgs/p3.jpg",
+        "happy birthday！": "./imgs/happy.jpg",
+        "新的一岁要吃好好的": "./imgs/new.jpg",
+        "喂饱饱的": "./imgs/eat.jpg",
+        "然后才有力气学习和生活！": "./imgs/study.jpg",
+        "生日快乐~~": "./imgs/flower.png"
+    },
+    
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点颜色",
+        balloons_flying: "好像还少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐～",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    // 结束语
+    loveText: '全世界最好的同桌-小梁同学'
+};
